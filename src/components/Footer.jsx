f4c29a99b1f2dd47f9df55b3097b556d8fe5c574
nelomelma <nelomelma74@gmail.com>
2026-05-04 22:17:53 +0600

    Add contact info in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,27 @@ export default function Footer() {
       style={{
         background: "#1f2937",
         color: "white",
-        padding: "30px 0",
+        padding: "30px 20px",
         textAlign: "center",
       }}
     >
+      {/* Title */}
       <h3 style={{ marginBottom: "10px" }}>QurbaniHat</h3>
 
+      {/* About */}
       <p style={{ marginBottom: "20px" }}>
         Your trusted platform for Qurbani livestock booking.
       </p>
 
+      {/* Contact Section */}
+      <div style={{ marginBottom: "20px" }}>
+        <h4>Contact</h4>
+        <p>Email: support@qurbanihat.com</p>
+        <p>Phone: +880 1700-000000</p>
+        <p>Location: Dhaka, Bangladesh</p>
+      </div>
+
+      {/* Social Links */}
       <div
         style={{
           display: "flex",
@@ -24,34 +35,20 @@ export default function Footer() {
           marginBottom: "20px",
         }}
       >
-        {/* Facebook */}
-        <a
-          href="https://facebook.com"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://facebook.com" target="_blank" rel="noreferrer">
           <FaFacebookF size={22} color="white" />
         </a>
 
-        {/* YouTube */}
-        <a
-          href="https://youtube.com"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://youtube.com" target="_blank" rel="noreferrer">
           <FaYoutube size={22} color="white" />
         </a>
 
-        {/* Instagram */}
-        <a
-          href="https://instagram.com"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://instagram.com" target="_blank" rel="noreferrer">
           <FaInstagram size={22} color="white" />
         </a>
       </div>
 
+      {/* Footer Bottom */}
       <p style={{ fontSize: "14px" }}>
         © {new Date().getFullYear()} QurbaniHat. All rights reserved.
       </p>
